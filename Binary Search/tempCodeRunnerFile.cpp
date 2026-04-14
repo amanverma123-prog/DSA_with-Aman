6eac3fc4ee20c852ecb10287e7d4e8b566482e74
Aman Verma <amanverma3911@gmail.com>
2026-04-14 17:15:34 +0530

    Implement LC 4: Median of Two Sorted Arrays

--- Binary Search/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-t findKthPositive(vector<int>& a, int k) {
-//     for (int i = 0; i < a.size(); i++) {
-//         if (a[i] < k)
-//             k++;
-//         else 
-//             break;
-//     }
-//     return k;
-// }
